6f05c17e16dfafe495a6d497b1fcc1677b8ac7c6
Administrator <Administrator@PC-20171115ONGG>
2019-12-02 17:01:37 +0800

    v1.1

--- abc/src/com/abc/test/test1.java
+++ abc/src/com/abc/test/test1.java
@@ -1,7 +1,12 @@
 package com.abc.test;
 
 public class test1 {
+	private void test() {
+		System.out.println("test void");
+	}
 	public static void main(String[] args) {
 		System.out.println("Git²âÊÔ´úÂë ");
 	}
+	
+	
 }
