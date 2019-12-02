4d3dce002ccb7e21e9090af193f34acc5b6eadf2
Administrator <Administrator@PC-20171115ONGG>
2019-12-02 16:57:39 +0800

    v2 branch

--- abc/src/com/abc/test/test1.java
+++ abc/src/com/abc/test/test1.java
@@ -1,7 +1,10 @@
 package com.abc.test;
 
+import java.util.Random;
+
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("Git²âÊÔ´úÂë ");
+		System.out.println("" + new Random().nextInt(100));
 	}
 }
